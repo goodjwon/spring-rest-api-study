5af2808e5edc08996e63a7a65b93bf53cba599cc
goodjwon <jwon76@naver.com>
2018-12-29 23:31:31 +0900

    spring docs 문서 작성

--- src/test/java/me/goodjwon/springrestapistudy/events/EventControllerTest.java
+++ src/test/java/me/goodjwon/springrestapistudy/events/EventControllerTest.java
@@ -2,16 +2,19 @@ package me.goodjwon.springrestapistudy.events;
 
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import me.goodjwon.springrestapistudy.common.RestDocsConfiguration;
 import me.goodjwon.springrestapistudy.common.TestDescription;
 import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -21,6 +24,10 @@ import org.springframework.test.web.servlet.MockMvc;
 import java.time.LocalDateTime;
 import java.util.regex.Matcher;
 
+import static org.springframework.restdocs.headers.HeaderDocumentation.*;
+import static org.springframework.restdocs.hypermedia.HypermediaDocumentation.*;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -35,6 +42,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
+@AutoConfigureRestDocs
+@Import(RestDocsConfiguration.class)
 public class EventControllerTest {
 
     @Autowired
@@ -75,8 +84,55 @@ public class EventControllerTest {
                 .andExpect(jsonPath("_links.self").exists())
                 .andExpect(jsonPath("_links.query-events").exists())
                 .andExpect(jsonPath("_links.update-event").exists())
+                .andDo(document(
+                        "create-event",
+                        links(halLinks(),
+                                linkWithRel("self").description("Link to self"),
+                                linkWithRel("query-events").description("Link to query events"),
+                                linkWithRel("update-event").description("Link to update event")
+                                ),
+                        requestHeaders(
+                                headerWithName(HttpHeaders.ACCEPT).description("accept header"),
+                                headerWithName(HttpHeaders.CONTENT_TYPE).description("contnetsType header")
+                        ),
+                        requestFields(
+                                fieldWithPath("name").description("이벤트명"),
+                                fieldWithPath("description").description("이벤트 설명"),
+                                fieldWithPath("beginEnrollmentDateTime").description("이벤트 접수시작 시간"),
+                                fieldWithPath("closeEnrollmentDateTime").description("이벤트 접수완료 시간"),
+                                fieldWithPath("beginEventDateTime").description("이벤트 시작 시간"),
+                                fieldWithPath("endEventDateTime").description("이벤트 종료 시간"),
+                                fieldWithPath("location").description("장소"),
+                                fieldWithPath("basePrice").description("최소 참가비용"),
+                                fieldWithPath("maxPrice").description("최대 참가비용"),
+                                fieldWithPath("limitOfEnrollment").description("마감여부")
+                        ),
+                        responseHeaders(
+                                headerWithName(HttpHeaders.LOCATION).description("Location header"),
+                                headerWithName(HttpHeaders.CONTENT_TYPE).description("Contents Type")
+                        ),
+                        responseFields(  // relaxedResponseFields 문서의 일부분만 체크, 정확한 문서를 만들지 못할 가능성이 있다. 주의 해서 사용.
+                                fieldWithPath("id").description("문서id"),
+                                fieldWithPath("name").description("이벤트명"),
+                                fieldWithPath("description").description("이벤트 설명"),
+                                fieldWithPath("beginEnrollmentDateTime").description("이벤트 접수시작 시간"),
+                                fieldWithPath("closeEnrollmentDateTime").description("이벤트 접수완료 시간"),
+                                fieldWithPath("beginEventDateTime").description("이벤트 시작 시간"),
+                                fieldWithPath("endEventDateTime").description("이벤트 종료 시간"),
+                                fieldWithPath("location").description("장소"),
+                                fieldWithPath("basePrice").description("최소 참가비용"),
+                                fieldWithPath("maxPrice").description("최대 참가비용"),
+                                fieldWithPath("limitOfEnrollment").description("마감여부"),
+                                fieldWithPath("free").description("무료인지여부"),
+                                fieldWithPath("offline").description("오프파인인지 여부"),
+                                fieldWithPath("eventStatus").description("이벤트 상태 표시"),
+                                fieldWithPath("_links.self.href").description("이벤트 상태 표시"),
+                                fieldWithPath("_links.query-events.href").description("이벤트 상태 표시"),
+                                fieldWithPath("_links.update-event.href").description("이벤트 상태 표시")
 
-        ;
+                        )
+
+                ));
 
     }
 
